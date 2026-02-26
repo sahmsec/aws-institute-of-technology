7f8e0697942f469e3c5c05703f34e9d92035d834
Sakib Ahmed <sakibahmedsadhin@gmail.com>
2026-02-27 04:54:19 +0600

    janina

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,15 +3,10 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  root: "client",
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client/src"),
+      "@": path.resolve(__dirname, "src"),
     },
-  },
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
   },
 });
